acbac3fbe93746aad85d90b484e573369c6d2a17
m0rn <morn88@gmail.com>
2016-06-06 15:23:44 +0300

    Добавлена функция построения графика, по полученным с Arduino данным

--- ardu/con_to_ardu.py
+++ ardu/con_to_ardu.py
@@ -1,10 +1,16 @@
 import serial
 import statistics
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+fig = plt.figure()
 
 PORT = 'com3'
 SPEED = 9600
-
 serial_connection = serial.Serial(PORT, SPEED)
+
+
 while True:
     line = serial_connection.readline().decode()
     line_s = line.split(' ')
@@ -13,6 +19,11 @@ while True:
     avr = statistics.mean(line_s)
     print("Для формы №:", form_number)
     print("Среднее натяжение:", avr)
+    ys = line_s
+    xs = [x for x in range(len(line_s))]
+    plt.plot(xs, ys)
+    plt.title("Form number: " + form_number)
+    plt.show()
     if form_number == '6666':
         break
 
