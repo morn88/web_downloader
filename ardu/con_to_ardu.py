0f8f6420276e3dd2bcaddca27bede6736b14ec9f
m0rn <morn88@gmail.com>
2016-06-06 14:06:13 +0300

    Изменен файл работы с Arduino по COM порту

--- ardu/con_to_ardu.py
+++ ardu/con_to_ardu.py
@@ -1,10 +1,20 @@
 import serial
+import statistics
 
 PORT = 'com3'
 SPEED = 9600
 
 serial_connection = serial.Serial(PORT, SPEED)
-for i in range(100):
-    print(serial_connection.readline())
+while True:
+    line = serial_connection.readline().decode()
+    line_s = line.split(' ')
+    form_number = line_s.pop()[:4]
+    line_s = [int(x) for x in line_s]
+    avr = statistics.mean(line_s)
+    print("Для формы №:", form_number)
+    print("Среднее натяжение:", avr)
+    if form_number == '6666':
+        break
 
 serial_connection.close()
+print("Работа окончена.")
